09049106cf611dab30255976c64e60cc11ba9ebd
AJ Phoenix <ajphnx_nu@unknown001f0150b4b0.attlocal.net>
2022-04-07 02:52:38 -0400

    finished project

--- public/ingredientList.js
+++ public/ingredientList.js
@@ -1,20 +1,22 @@
+const ingredientList = {
+
 // The array of ingredients the user can add to a sandwich
 //  This will be updated after we fetch.
-let ingredients = [];
+    ingredients : [],
 
 // Updates the DOM to display a list of ingredients
-function renderIngredientList() {
-    const ingredientUl = document.querySelector('.ingredient-list');
+    render() {
+        const ingredientUl = document.querySelector('.ingredient-list');
 
-    // Empty the ingredientList before adding any content to it.
-    ingredientUl.innerHTML = '';
+        // Empty the ingredientList before adding any content to it.
+        ingredientUl.innerHTML = '';
 
-    ingredients.forEach(ingredient => {
-        const ingredientDiv = renderIngredientCard(ingredient);
-        ingredientUl.append(ingredientDiv)
-    })
+        ingredients.forEach(ingredient => {
+            const ingredientDiv = renderIngredientCard(ingredient);
+            ingredientUl.append(ingredientDiv)
+        })
+    }
 }
-
 // Creates a DIV to display a single ingredient
 function renderIngredientCard(ingredient) {
     const ingredientCard = document.createElement('div');
@@ -42,7 +44,7 @@ function renderIngredientCard(ingredient) {
     `
     const toggleButton = ingredientCard.querySelector('.toggle-button')
     toggleButton.addEventListener('click', () => {
-        toggleIngredient(ingredient)
+            this.toggleIngredient(ingredient)
     })
 
     return ingredientCard
@@ -59,5 +61,5 @@ function toggleIngredient(ingredient) {
     }
     saveSelectedSandwich()
     renderCart()
-    renderIngredientList()
+    ingredientList.render()
 }
